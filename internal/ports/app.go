e6ffd3e77a9497550ffd2369dc15a682df9cba82
unreal-kz <ukibas.b@gmail.com>
2022-07-01 15:15:28 +0300

    app.go file has been changed

--- internal/ports/app.go
+++ internal/ports/app.go
@@ -4,5 +4,5 @@ type APIPort interface {
 	GetAddition(a, b int32) (int32, error)
 	GetSubtraction(a, b int32) (int32, error)
 	GetMultiplication(a, b int32) (int32, error)
-	GetDevision(a, b int32) (int32, error)
+	GetDivision(a, b int32) (int32, error)
 }
